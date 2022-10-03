1aa9a6a45ce9c59ea60036a334e332573a31c843
mario junior <mario_10gt@hotmail.com>
2022-10-03 11:21:08 -0300

    feat: reorganized directory structure and added more routers, controllers and useCases

--- typescript/src/routes/categories.routes.ts
+++ typescript/src/routes/categories.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
+import multer from "multer";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/ListCategories";
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (req, res) => {
   return createCategoryController.handle(req, res);
@@ -10,6 +16,10 @@ categoriesRoutes.post("/", (req, res) => {
 
 categoriesRoutes.get("/", (_, res) => {
   return listCategoriesController.handle(res);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+  return importCategoryController.handle(req, res);
 });
 
 export { categoriesRoutes };
